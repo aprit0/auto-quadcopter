23a6a4d4eb20c3aa19b4c96ce11ec3fa70f0c4e7
aprit <a.pritchard@outlook.com>
2022-09-19 10:07:02 +0000

    MW 2

--- Drone/multi_wii.py
+++ Drone/multi_wii.py
@@ -3,17 +3,19 @@ from Comms.pymultiwii import MultiWii
 class MW:
     def __init__(self):
         self.board = MultiWii("/dev/ttyUSB0")
+
         self.motor = {'m1': 0, 'm2': 0, 'm3': 0, 'm4': 0, 'elapsed': 0, 'timestamp': 0}
         self.rcChannels = {'roll': 0, 'pitch': 0, 'yaw': 0, 'throttle': 0, 'elapsed': 0, 'timestamp': 0}
         self.altitude = {'estalt': 0, 'vario': 0, 'elapsed': 0, 'timestamp': 0}
 
-    def update_mw(self, data=[1000]*8):
-        # self.board.sendCMDreceiveATT(16, MultiWii.SET_RAW_RC, data)
-        self.sendCMD(8, MultiWii.SET_RAW_RC, data[:4])
+    def update_mw(self, data=[1500]*8):
+        self.board.getData(MultiWii.ATTITUDE)
         self.motor = self.board.motor
         self.rcChannels = self.board.rcChannels
-        self.altitude = self.board.rcChannels
-
+        self.attitude = self.board.attitude
+        print(self.board.attitude)
+        self.board.sendCMD(16, MultiWii.SET_RAW_RC, data[:3], 'i')
+        print(self.board.attitude)
 if __name__ == "__main__":
     FC = MW()
     while True:
