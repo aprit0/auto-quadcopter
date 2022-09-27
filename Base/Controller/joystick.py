db72b077e67068a8e004bc63bcf43e0146df1d41
aprit0 <a.pritchard@outlook.com>
2022-09-27 22:08:27 +1000

    added keyboard

--- Base/Controller/joystick.py
+++ Base/Controller/joystick.py
@@ -1,4 +1,6 @@
 import pygame
+import numpy as np
+from copy import deepcopy
 '''
 Aim: General controller class for ROS
 - Not ROS connected
@@ -9,77 +11,91 @@ Functions
 See update_state for XBOX one Controller keymaps
 '''
 
-class Joystick():
-    def __init__(self):
-        self.state = {'axes': [0.]*2 + [-1.] + [0.]*2 + [-1.],
-                      'buttons': [0]*11}
+
+class Joystick:
+    def __init__(self, Keyboard=True, MVA=False):
+        self.reset = {'axes': [0.] * 2 + [-1.] + [0.] * 2 + [-1.],
+                      'buttons': [0.] * 11}
+        self.MVA = MVA
+        self.Key = Keyboard
+        self.state = deepcopy(self.reset)
+
+    def filter(self, old_axes):
+        new_axes = self.state['axes']
+        split = [0.8, 0.2]  # moving average
+        return [split[1] * new_axes[i] + split[0] * old_axes[i] for i in range(len(old_axes))]
 
     def update_state(self, events):
+        old_state = deepcopy(self.state)
         for event in events:
-            if event.type == pygame.JOYBUTTONDOWN:
-                # Button
-                num = event.button
-                self.state['buttons'][num] = 0 if self.state['buttons'][num] != 0 else 1
-                if num == 0:
-                    # A pressed
-                    pass
-                elif num == 1:
-                    # B pressed
-                    pass
-                elif num == 2:
-                    # X pressed
-                    pass
-                elif num == 3:
-                    # Y pressed
-                    pass
-                elif num == 4:
-                    # LB pressed
-                    pass
-                elif num == 5:
-                    # RB pressed
-                    pass
-                elif num == 6:
-                    # 2 Squares pressed
-                    pass
-                elif num == 7:
-                    # Hamburger pressed
-                    pass
-                elif num == 8:
-                    # XBOX pressed
-                    pass
-                elif num == 9:
-                    # Left Joy pressed
-                    pass
-                elif num == 10:
-                    # Right Joy pressed
-                    pass
-                else:
-                    pass
-            elif event.type == pygame.JOYAXISMOTION:
-                # Joystick: [-1, 1] maps to [Left, Right] and [Up, Down]
-                # Trigger: [-1, 1] maps to [0, depressed]
-                axes = event.axis
-                self.state['axes'][axes] = event.value
-                if axes == 0:
-                    # LX pressed
-                    pass
-                elif axes == 1:
-                    # LY pressed
-                    pass
-                elif axes == 2:
-                    # LT pressed
-                    pass
-                elif axes == 3:
-                    # LX pressed
-                    pass
-                elif axes == 4:
-                    # LY pressed
-                    pass
-                elif axes == 5:
-                    # RT pressed
-                    pass
-            elif event.type == pygame.JOYHATMOTION:
-                # xy buttons
-                pass
+            if self.Key:
+                self.get_keyboard(event)
             else:
-                pass
+                self.get_joystick(event)
+        if self.MVA:
+            self.state['axes'] = self.filter(old_state['axes'])
+        return self.input_to_pwm()
+
+    def input_to_pwm(self):
+        for key in list(self.state.keys()):
+            self.state[key] = np.clip(self.state[key], -1, 1)
+        state = deepcopy(self.reset)
+        state['axes'] = np.interp(list(self.state['axes']), (-1, 1), (1000, 2000))
+        state['buttons'] = np.interp(list(self.state['buttons']), (0, 1), (1000, 2000))
+        return state
+
+    def get_keyboard(self, event):
+        # Limited from -1 to 1
+        STEP = 0.2
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            self.state['axes'][0] += STEP
+        elif keys[pygame.K_DOWN]:
+            self.state['axes'][0] -= STEP
+        elif keys[pygame.K_1]:
+            self.state['buttons'][0] = 1
+        elif keys[pygame.K_2]:
+            self.state['buttons'][1] = 1
+        elif keys[pygame.K_0]:
+            self.state = deepcopy(self.reset)
+        else:
+            pass
+
+    def get_joystick(self, event):
+        # Limited from -1 to 1
+        if event.type == pygame.JOYBUTTONDOWN:
+            '''
+            BUTTON
+            Number: Key
+            0: A
+            1: B
+            2: X
+            3: Y
+            4: LB
+            5: Rb
+            6: 2Squares
+            7: Hamburger
+            8: Xbox
+            9: LeftJoy
+            10: RightJoy
+            '''
+            num = event.button
+            self.state['buttons'][num] = 0 if self.state['buttons'][num] != 0 else 1
+        elif event.type == pygame.JOYAXISMOTION:
+            # Joystick: [-1, 1] maps to [Left, Right] and [Up, Down]
+            # Trigger: [-1, 1] maps to [0, depressed]
+            '''
+            0: LX
+            1: LY
+            2: LT
+            3: RX
+            4: RY
+            5: RT
+            '''
+            axes = event.axis
+            self.state['axes'][axes] = event.value
+        elif event.type == pygame.JOYHATMOTION:
+            # xy buttons
+            pass
+        else:
+            pass
--- Base/Controller/joystick_node.py
+++ Base/Controller/joystick_node.py
@@ -7,34 +7,39 @@ import pygame
 
 
 class JoystickNode(Node):
-    def __init__(self):
+    def __init__(self, args):
         super().__init__('joystick_mode')
         self.pub_joy = self.create_publisher(Joy, 'base/joy', 10)
         timer_period = 0.1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.i = 0
-        self.Joy = Joystick()
+        self.Joy = Joystick(Keyboard=args['Key'])
 
     def timer_callback(self):
         events = pygame.event.get()
         if events and not (len(events) == 1 and events[0].type == pygame.JOYBUTTONUP):
-            self.Joy.update_state(events)
+            state = self.Joy.update_state(events)
             msg = Joy()
             msg.header = Header()
-            msg.axes = self.Joy.state['axes']
-            msg.buttons = self.Joy.state['buttons']
+            msg.header.stamp = self.get_clock().now().to_msg()
+            msg.axes = [float(i) for i in state['axes']]
+            msg.buttons = [int(i) for i in state['buttons']]
             self.pub_joy.publish(msg)
             self.get_logger().info('{}'.format(msg))
 
 
-def main(args=None):
+def main(args={'Key': True}):
     pygame.init()
-    while not pygame.joystick.get_count():
+    while not args['Key'] and not pygame.joystick.get_count():
         print('No joystick detected', pygame.joystick.get_count())
-    init_joystick = pygame.joystick.Joystick(0)
-    init_joystick.init()
-    rclpy.init(args=args)
-    joy_publisher = JoystickNode()
+    if not args['Key']:
+        init_joystick = pygame.joystick.Joystick(0)
+        init_joystick.init()
+    windowSize = width, height = 8, 6
+    screen = pygame.display.set_mode(windowSize)
+
+    rclpy.init()
+    joy_publisher = JoystickNode(args)
     rclpy.spin(joy_publisher)
     joy_publisher.destroy_node()
     rclpy.shutdown()
