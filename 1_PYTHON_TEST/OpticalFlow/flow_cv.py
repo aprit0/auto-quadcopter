10ae2ab18ac868701ac20cb198f1a0f704724795
aprit <a.pritchard@outlook.com>
2023-01-22 11:48:30 +1100

    optical flow calcs

---
+++ 1_PYTHON_TEST/OpticalFlow/flow_cv.py
@@ -0,0 +1,69 @@
+import cv2 as cv
+import numpy as np
+import matplotlib as plt
+
+
+def main():
+    cap = cv.VideoCapture(0)
+    ret, first_frame = cap.read()
+    prev_gray = cv.cvtColor(first_frame, cv.COLOR_BGR2GRAY)
+
+    mask = np.zeros_like(first_frame)
+
+    # Sets image saturation to maximum
+    mask[..., 1] = 255
+
+    while (cap.isOpened()):
+
+        # ret = a boolean return value from getting
+        # the frame, frame = the current frame being
+        # projected in the video
+        ret, frame = cap.read()
+
+        # Opens a new window and displays the input
+        # frame
+        cv.imshow("input", frame)
+
+        # Converts each frame to grayscale - we previously
+        # only converted the first frame to grayscale
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+        # Calculates dense optical flow by Farneback method
+        flow = cv.calcOpticalFlowFarneback(prev_gray, gray,
+                                           None,
+                                           0.5, 3, 15, 3, 5, 1.2, 0)
+
+        # Computes the magnitude and angle of the 2D vectors
+        magnitude, angle = cv.cartToPolar(flow[..., 0], flow[..., 1]) # magnitude, angle = (480, 640)
+        print(magnitude[:10, :10])
+        # Sets image hue according to the optical flow
+        # direction
+        mask[..., 0] = angle * 180 / np.pi / 2
+
+        # Sets image value according to the optical flow
+        # magnitude (normalized)
+        mask[..., 2] = cv.normalize(magnitude, None, 0, 255, cv.NORM_MINMAX)
+
+        # Converts HSV to RGB (BGR) color representation
+        rgb = cv.cvtColor(mask, cv.COLOR_HSV2BGR)
+
+        # Opens a new window and displays the output frame
+        cv.imshow("dense optical flow", rgb)
+
+        # Updates previous frame
+        prev_gray = gray
+
+        # Frames are read by intervals of 1 millisecond. The
+        # programs breaks out of the while loop when the
+        # user presses the 'q' key
+        if cv.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # The following frees up resources and
+    # closes all windows
+    cap.release()
+    cv.destroyAllWindows()
+
+
+if __name__ == '__main__':
+    main()
