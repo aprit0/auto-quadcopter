3e12b91d432ee73f2fcbb011046d0926e61fed5c
aprit0 <a.pritchard@outlook.com>
2023-08-22 17:26:19 +1000

    add ps5 controller

--- Base/controllers/controller_test.py
+++ Base/controllers/controller_test.py
@@ -1,22 +1,33 @@
 import pygame
 import time
 
-from xbox_wireless import CONTROLLER
+# from xbox_wireless import CONTROLLER
+from ps5_wireless import CONTROLLER
 
 def test(joy, cont):
     event_new = pygame.event.get()
     # joysticks
     axes = [joy.get_axis(i) for i in range(joy.get_numaxes())]
     # cont.map(axes, None, None)
-    print({i: axes[i] for i in range(len(axes))})
+    # print("axes: ", {i: axes[i] for i in range(len(axes))})
     # print(cont.cartesian_axes, cont.trigger_axes)
 
     # Buttons
     buttons = [joy.get_button(i) for i in range(joy.get_numbuttons())]
     hats = [joy.get_hat(i) for i in range(joy.get_numhats())]
-    # print({i: buttons[i] for i in range(len(buttons))}, hats)
-    cont.map(None, buttons, hats[0])
-    # print(cont.buttons)
+    # print("buttons: ", {i: buttons[i] for i in range(len(buttons))}, hats)
+
+    cont.map(axes, buttons, hats[0])
+    print(cont.cartesian_axes)
+    print(cont.trigger_axes)
+    print(cont.buttons)
+
+def stats(joy):
+    print("name: ",joy.get_name())
+    print("power: ",joy.get_power_level())
+    print("buttons: ",joy.get_numbuttons())
+    print("hats: ",joy.get_numhats())
+    print("axes: ",joy.get_numaxes())
 
 if __name__ == '__main__':
     cont = CONTROLLER()
@@ -26,6 +37,7 @@ if __name__ == '__main__':
         print('No Controller detected')
     joy = pygame.joystick.Joystick(0)
     joy.init()
+    stats(joy)
     while True:
         test(joy, cont)
         time.sleep(0.1)
