39df77b1c6b432ced12340f0ea8da1cccc478650
aprit0 <a.pritchard@outlook.com>
2023-12-17 15:33:52 +1100

    base

--- setup.sh
+++ setup.sh
@@ -10,4 +10,6 @@ sudo apt install iwlist
 ## ROS
 sudo apt install ros-$ROS_DISTRO-foxglove-bridge
 echo -e "\nalias foxglove='ros2 launch foxglove_bridge foxglove_bridge_launch.xml'" >> ~/.bashrc
+echo -e "alias drone'cd ~/ros2_ws/src/auto-quadcopter/auto-quadcopter/Drone'"
+echo -e "alias base'cd ~/ros2_ws/src/auto-quadcopter/auto-quadcopter/Base'"
 
