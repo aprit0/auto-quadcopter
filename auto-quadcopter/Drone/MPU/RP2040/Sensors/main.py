56e645ea9f40261f7b862b7f068c86d01b6d3ccc
aprit <a.pritchard@outlook.com>
2024-03-07 17:30:54 +1100

    pico main edit

--- auto-quadcopter/Drone/MPU/RP2040/Sensors/main.py
+++ auto-quadcopter/Drone/MPU/RP2040/Sensors/main.py
@@ -7,40 +7,40 @@ from mpl3115a2 import MPL3115A2
 import asyncio
 from time import monotonic
 
-class Altitude(MPL3115A2):
-    def __init__(self, i2c):
-        self.old_altitude = 0
-        super().__init__(i2c)
+# class Altitude(MPL3115A2):
+#     def __init__(self, i2c):
+#         self.old_altitude = 0
+#         super().__init__(i2c)
 
-    def calibrate(self, n_trys=5):
-        ave_pressure = 0
-        for i in range(n_trys):
-            ave_pressure += self.blocking_read(self.pressure)
-        ave_pressure = int(ave_pressure / n_trys)
-        self.sealevel_pressure = ave_pressure
-        return self.read_altitude(blocking=True), ave_pressure
+#     def calibrate(self, n_trys=5):
+#         ave_pressure = 0
+#         for i in range(n_trys):
+#             ave_pressure += self.blocking_read(self.pressure)
+#         ave_pressure = int(ave_pressure / n_trys)
+#         self.sealevel_pressure = ave_pressure
+#         return self.read_altitude(blocking=True), ave_pressure
 
-    def blocking_read(self, func, n_trys=0.5):
-        t_0 = monotonic()
-        while monotonic() - t_0 < n_trys:
-            out = func()
-            if out:
-                break
-        if out:
-            return out
-        else:
-            raise Exception(f"{self.__class__.__name__}: Blocking Read failed {i}/{n_trys-1}: out: {out}")\
+#     def blocking_read(self, func, n_trys=0.5):
+#         t_0 = monotonic()
+#         while monotonic() - t_0 < n_trys:
+#             out = func()
+#             if out:
+#                 break
+#         if out:
+#             return out
+#         else:
+#             raise Exception(f"{self.__class__.__name__}: Blocking Read failed {i}/{n_trys-1}: out: {out}")\
     
-    def read_altitude(self, blocking=False):
-        if blocking:
-            out = self.blocking_read(self.altitude)
-        else:
-            out = self.altitude()
-        if out:
-            self.old_altitude = out
-            return out, True
-        else:
-            return self.old_altitude, False
+#     def read_altitude(self, blocking=False):
+#         if blocking:
+#             out = self.blocking_read(self.altitude)
+#         else:
+#             out = self.altitude()
+#         if out:
+#             self.old_altitude = out
+#             return out, True
+#         else:
+#             return self.old_altitude, False
             
 
 class State:
@@ -58,16 +58,16 @@ class Sensors(State):
     def __init__(self) -> None:
         super().__init__()
         self.i2c = busio.I2C(board.GP3, board.GP2)
-        self.alt = Altitude(self.i2c)
+        # self.alt = Altitude(self.i2c)
         self.bno = BNO08X_I2C(self.i2c)
         self.bno.enable_feature(BNO_REPORT_ACCELEROMETER)
-        self.println(self.alt.calibrate())
+        # self.println(self.alt.calibrate())
 
     def get_acceleration(self):
         return self.bno.acceleration
     
-    def get_altitude(self):
-        return self.alt.read_altitude()[0]
+    # def get_altitude(self):
+    #     return self.alt.read_altitude()[0]
     
 
 
@@ -76,23 +76,19 @@ class Drone(Sensors):
         super().__init__()
         self.println(self.__dict__)
 
-def main():
+def loop():
     robot = Drone()
 
     while True:
         t_0 = monotonic()
-        loop(robot)
+        accel_x, accel_y, accel_z = robot.get_acceleration()
+        robot.acc = [accel_x, accel_y, accel_z] 
+        print("Run", robot.pose, robot.acc)
         print(1/ (monotonic() - t_0))
 
 
-def loop(robot):
-    robot.pose[-1] = robot.get_altitude()
-    accel_x, accel_y, accel_z = robot.get_acceleration()
-    robot.acc = [accel_x, accel_y, accel_z] 
-    print("Run", robot.pose, robot.acc)
-
 if __name__ == "__main__":
-    main()
+    loop()
 
 
 # async def read_slow(robot):
